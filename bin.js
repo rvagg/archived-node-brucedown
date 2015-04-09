6f6ec70c61b10ba8c06d76385ba79955d4f47a5a
Tim Oxley <secoif@gmail.com>
2015-04-09 17:55:25 +0800

    Print to stdout if output file not supplied.

--- bin.js
+++ bin.js
@@ -5,12 +5,21 @@ const brucedown = require('./')
 
 function usageAndExit (code) {
   if (code == null) code = -1
-  console.error('Usage: brucedown <markdown file> <output file>')
+  console.error('Usage: brucedown <markdown file> [output file]')
   process.exit(code)
 }
 
 if (process.argv[2] === '--help') {
   return usageAndExit(0)
+}
+
+if (process.argv.length === 3) {
+  brucedown(fs.readFileSync(process.argv[2], 'utf8'), function (err, data) {
+    if (err)
+      throw err
+    process.stdout.write(data + '\n')
+  })
+  return
 }
 
 if (process.argv.length < 4)
